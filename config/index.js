aa71d9cafde1705e6d966a26877b29c98c973fb3
亦知 <79632357+hl1206905735@users.noreply.github.com>
2022-09-14 17:42:30 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -23,9 +23,9 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "亦知",
+      name: "否",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oSj9s5qjg378s_NaG05oKatR0HEE",
+      id: "oSj9s5owzkpFHH5v-_eejGmx7OJk",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "SHidQIEy6G-0a4kGVf8bLFy2T_pN4miZ3y_xJMoE2lo",
       // 所在省份
@@ -62,6 +62,44 @@ export const config = {
    
     // 你可以不断按格式往下增加
     // ...
+      {
+      // 想要发送的人的名字
+      name: "亦知",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "oSj9s5qjg378s_NaG05oKatR0HEE",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "SHidQIEy6G-0a4kGVf8bLFy2T_pN4miZ3y_xJMoE2lo",
+      // 所在省份
+      province: "广东",
+      // 所在城市
+      city: "深圳",
+      // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '08-21',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://www.fakeupdate.net/",
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {"type": "*生日", "name": "平宝", "year": "2000", "date": "07-22"},
+       {"type": "节日", "name": "除夕", "year": "2023", "date": "01-21"},
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+     //  {"type": "*生日", "name": "小胡", "year": "1999", "date": "09-25"},
+        {"type": "节日", "name": "国庆", "year": "2022", "date": "10-01"},
+           {"type": "节日", "name": "圣诞节", "year": "2022", "date": "12-25"},
+          {"type": "节日", "name": "2022年的结束", "year": "2023", "date": "01-01"},
+      ],
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      customizedDateList: [
+        // 在一起的日子
+       // {"keyword": "love_day", date: "2022-09-08"},
+        // 结婚纪念日
+      //  {"keyword": "marry_day", date: "2022-09-09"},
+        // 退伍日
+       // {"keyword": "ex_day", date: "2022-09-10"},
+      ]
+    },
   ],
 
   /**
